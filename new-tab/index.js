fb2455c6411f1fdd059305829d6ee3dcad2e508c
zainsci <60652827+zainsci@users.noreply.github.com>
2021-02-05 00:40:46 +0500

    Add JS to add Random Image

--- new-tab/index.js
+++ new-tab/index.js
@@ -1,6 +1,13 @@
+// Add Random Colors to Boxes
 const colors = ["red", "yellow", "blue"];
 let boxes = document.querySelectorAll(".box");
 
 boxes.forEach((box) => {
   box.classList.add(colors[Math.floor(Math.random() * 3)]);
 });
+
+// Add Random Images from Media Folder
+const image = document.getElementById("image");
+const img = document.createElement("img");
+img.src = `../media/images/${"1"}.jpg`;
+image.appendChild(img);
