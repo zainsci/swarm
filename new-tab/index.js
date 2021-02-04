e4ebfce7a3fe48bd8cd10e774f7606cd258dbfad
zainsci <60652827+zainsci@users.noreply.github.com>
2021-02-05 01:35:02 +0500

    Add Function to Add Radnom Vocab

--- new-tab/index.js
+++ new-tab/index.js
@@ -1,3 +1,6 @@
+const noOfKanjiVocabFiles = 1030;
+const noOfImages = 17;
+
 // Add Random Colors to Boxes
 const colors = ["red", "yellow", "blue"];
 let boxes = document.querySelectorAll(".box");
@@ -9,5 +12,30 @@ boxes.forEach((box) => {
 // Add Random Images from Media Folder
 const image = document.getElementById("image");
 const img = document.createElement("img");
-img.src = `../media/images/${Math.floor(Math.random() * 17)}.jpg`;
+img.src = `../media/images/${Math.floor(Math.random() * noOfImages)}.jpg`;
 image.appendChild(img);
+
+// Add Random Kanji Vocab To Page
+fetch(`../media/vocab/${Math.floor(Math.random() * noOfKanjiVocabFiles)}.json`)
+  .then((data) => data.json())
+  .then((data) => {
+    addVocab(data);
+  });
+
+function addVocab(data) {
+  const vocabDiv = document.getElementById("kanji");
+  const h2 = document.createElement("h2");
+  const div = document.createElement("div");
+  const h4 = document.createElement("h4");
+  const p = document.createElement("p");
+
+  h2.innerHTML = data.vocab;
+  h4.innerHTML = data.hiragana;
+  p.innerHTML = data.english;
+
+  div.appendChild(h4);
+  div.appendChild(p);
+
+  vocabDiv.appendChild(h2);
+  vocabDiv.appendChild(div);
+}
