0953989fc1c6603befe0bf44043112e321af6e81
zainsci <60652827+zainsci@users.noreply.github.com>
2021-03-05 17:55:13 +0500

    Upgrade to ManivestVer3

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.action.onClicked.addListener(playAudio);
 
 function playAudio() {
   const audioURL = Math.round(Math.random() * noOfAudios);
-  const audio = new Audio(`../media/audio/${audioURL}.mp3`);
+  const audio = new Audio(`./media/audio/${audioURL}.mp3`);
 
   audio.play();
 }
